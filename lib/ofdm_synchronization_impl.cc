9716f3b4a6ab5196d65cd1eaa08cb2d8a795986a
git-artes <flarroca@fing.edu.uy>
2021-11-23 16:38:21 -0300

    removed set_align, see how it goes

--- lib/ofdm_synchronization_impl.cc
+++ lib/ofdm_synchronization_impl.cc
@@ -118,8 +118,8 @@ namespace gr {
                   //VOLK alignment as recommended by GNU Radio's Manual. It has a similar effect 
                   //than set_output_multiple(), thus we will generally get multiples of this value
                   //as noutput_items. 
-                  const int alignment_multiple = volk_get_alignment() / sizeof(gr_complex);
-                  set_alignment(std::max(1, alignment_multiple));
+                  //const int alignment_multiple = volk_get_alignment() / sizeof(gr_complex);
+                  //set_alignment(std::max(1, alignment_multiple));
 
                   d_align = volk_get_alignment();
 
